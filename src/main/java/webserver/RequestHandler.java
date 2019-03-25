de86f353806dff032d999b08f5a0fc19e34afd46
moon <mf311110@gmail.com>
2019-03-25 15:59:55 +0900

    get->post

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,10 +5,14 @@ import java.net.Socket;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.util.Map;
 //what is InputStreamReader????
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -30,6 +34,7 @@ public class RequestHandler extends Thread {
             //요청라인 -- http 규약에서 맨 첫 번째 줄은 무조건 요청 라인이다.
             String line = br.readLine();
             log.debug("request line : {}",line);
+
             if (line ==null){
                 return;
             }
@@ -37,19 +42,52 @@ public class RequestHandler extends Thread {
             //요청헤더 -- 2번째 줄부터 빈 문자열이 나올때 까지가 전부 http 요청 헤더에 포함된다
             //여기서는 클라이언트가 서버에 보내는 http 요청 헤더이다.
             String[] tokens = line.split(" ");
+            int contentLength = 0;
             while(!line.equals("")){//빈 문자열이 나올 때 까지 요청헤더를 읽어들인다.
-                line = br.readLine();
                 log.debug("header : {}",line);
+                line = br.readLine();
+                if(line.contains("Content-Length")){
+                    contentLength = getContentLength(line);
+                }
             }
 
-            //요청본문 -- 공백 문자열 다음 줄부터 원하는 경로에서 읽어들이고자 하는 문서를 한 줄씩 읽어들이면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
+//            //(get)회원가입 요청url이 있을 시 파싱해서 map에 저장
+//            String url = tokens[1];
+//            if(url.startsWith("/user/create")){
+//                int index = url.indexOf("?");
+//                String queryString = url.substring(index+1);
+//                Map<String, String>params = HttpRequestUtils.parseQueryString(queryString);
+//                User user = new User(params.get("userId"),params.get("password"),params.get("name"),params.get("email"));
+//                log.debug("User : {}", user);
+//            }else{
+//                //요청본문 -- 공백 문자열 다음 줄부터 원하는 경로에서 읽어들이고자 하는 문서를 한 줄씩 읽어들이면 된다.
+//                DataOutputStream dos = new DataOutputStream(out);
+//                byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
+//
+//                //서버에서 클라이언트에 보내는 response
+//                // 이것 또한 분석해보면 1줄은상태라인/2줄부터 공백라인까지 응답헤더, 그 이후부터 응답 본문이 적혀있다.
+//                response200Header(dos, body.length);
+//                responseBody(dos,body);
+//
+//            }
+            //(post)회원가입 요청url이 있을 시 파싱해서 map에 저장
+            String url = tokens[1];
+            if(("/user/create".equals(url))){
+                String body = IOUtils.readData(br,contentLength);
+                Map<String, String>params = HttpRequestUtils.parseQueryString(body);
+                User user = new User(params.get("userId"),params.get("password"),params.get("name"),params.get("email"));
+                log.debug("User : {}", user);
+            }else{
+                //요청본문 -- 공백 문자열 다음 줄부터 원하는 경로에서 읽어들이고자 하는 문서를 한 줄씩 읽어들이면 된다.
+                DataOutputStream dos = new DataOutputStream(out);
+                byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
 
-            //서버에서 클라이언트에 보내는 response
-            // 이것 또한 분석해보면 1줄은상태라인/2줄부터 공백라인까지 응답헤더, 그 이후부터 응답 본문이 적혀있다.
-            response200Header(dos, body.length);
-            responseBody(dos,body);
+                //서버에서 클라이언트에 보내는 response
+                // 이것 또한 분석해보면 1줄은상태라인/2줄부터 공백라인까지 응답헤더, 그 이후부터 응답 본문이 적혀있다.
+                response200Header(dos, body.length);
+                responseBody(dos,body);
+
+            }
 
         } catch (IOException e) {
             log.error(e.getMessage());
@@ -74,5 +112,10 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private int getContentLength(String line){
+        String[] headerTokens = line.split(":");
+        return Integer.parseInt(headerTokens[1].trim());
     }
 }
